e5ff69a6a565a1ffa2d95f725eac23ae3b4b980f
Michaelangelo Ramos <ramos.michael85@gmai.com>
2019-07-13 19:29:10 +0000

    Added @ to quote

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -2,9 +2,10 @@ class QuotesControler < ApplicationController
   def index
     @quote = Quote.order("RANDOM()").first 
   end
+  
 
   def create
-    @quote = Quote.create(quote.params)
+    @quote = Quote.create(quote_params)
     if @quote.invalid?
         flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
       end  
